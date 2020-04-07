8b99065c09c749213019c6ea04b6eb6be9edb5f0
Artem Tenkov <codefiredeveloper@gmail.com>
2020-04-07 22:49:50 +0200

    Workout schedule view: - unit tests added - component added

--- fitness-app/components/booking.js
+++ fitness-app/components/booking.js
@@ -3,3 +3,10 @@ import React from 'react';
 export const Booking = ({user}) => (
     <div>{user.firstName}</div>
 );
+
+export const WorkoutScheduleView = ({workouts}) => (
+    <ol id="shedule_view">
+     {workouts.map(workout => (
+        <li key={workout.startsAt}>{workout.startsAt}</li>))
+     }
+    </ol>)
--- fitness-app/test/Booking.test.js
+++ fitness-app/test/Booking.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  { Booking } from '../components/booking.js';
+import  { Booking, WorkoutScheduleView } from '../components/booking.js';
 
 //NB! Never refactor red tests!
 
@@ -33,4 +33,53 @@ describe('Booking page', () => {
         //Assert
         expect(container.textContent).toMatch('Pavel');
     });
+})
+
+describe('Workouts schedule view', () => {
+    let container;
+    beforeEach(() => {
+        container = document.createElement('div');
+    })
+
+    const today = new Date();        
+    const workoutTimeFormat = (time) => {
+        const [h, m] = new Date(time).toTimeString().split(':');
+        return `${h}:${m}`;
+    };
+    const workouts = [
+        {startsAt: workoutTimeFormat(today.setHours(12, 0))},
+        {startsAt: workoutTimeFormat(today.setHours(13, 0))}
+    ];
+
+    const render = component =>
+        ReactDOM.render(component, container);
+
+    it('renders an ol with a right id', () => {
+        //Act
+        render(<WorkoutScheduleView 
+            workouts={ workouts } />);
+
+        //Assert
+        expect(container.querySelector('ol#shedule_view')).not.toBeNull();
+    })
+
+    it('renders multiple bookings in ol list', ()=> {
+        // Act
+        render(<WorkoutScheduleView workouts={workouts} />);
+
+        //Assert
+        expect(container.querySelector('ol').children)
+            .toHaveLength(2);
+    })    
+
+    it('renders each workout in a li', () => {
+        
+        // Act
+        render(<WorkoutScheduleView workouts={workouts} />);
+
+        //Assert
+        expect(container.querySelectorAll('li')).toHaveLength(2);
+        expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+        expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+    })
 })
