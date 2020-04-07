f484a764fab6feb0d94f9db75a447e9becb573a9
Artem Tenkov <codefiredeveloper@gmail.com>
2020-04-07 19:39:09 +0200

    Unit tests refactoring

--- fitness-app/components/booking.js
+++ fitness-app/components/booking.js
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export const Booking = () => <div>Artem</div>
+export const Booking = ({user}) => (
+    <div>{user.firstName}</div>
+);
--- fitness-app/test/Booking.test.js
+++ fitness-app/test/Booking.test.js
@@ -2,15 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import  { Booking } from '../components/booking.js';
 
-describe('Booking page', () => {    
+//NB! Never refactor red tests!
+
+describe('Booking page', () => {
+    let container;
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    const render = (component) => ReactDOM.render(component, container);     
+
     it('Renders user\'s first name', ()=> {
+        //Arrange
         const user = { firstName: 'Artem' };
-        const bookingComponent = <Booking user={user} />;
 
-        var container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(bookingComponent, container);       
+        //Act
+        render(<Booking user={user} />);
 
-        expect(document.body.textContent).toMatch('Artem');
+        //Assert
+        expect(container.textContent).toMatch('Artem');
+    });
+
+    it('Renders first name of another user', ()=> {
+        //Arrange
+        const user = { firstName: 'Pavel' };
+
+        //Act
+        render(<Booking user={user} />);
+
+        //Assert
+        expect(container.textContent).toMatch('Pavel');
     });
 })
