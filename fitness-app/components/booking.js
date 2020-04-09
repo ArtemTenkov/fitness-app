defbe073fa4011f9e9bfb0dd7688fc7ad45026e6
Artem Tenkov <codefiredeveloper@gmail.com>
2020-04-09 19:11:08 +0200

    - Initial workout behaviour functionality added - Code refactoring

--- fitness-app/components/booking.js
+++ fitness-app/components/booking.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-export const Booking = ({user}) => (
-    <div>{user.firstName}</div>
+export const Booking = ({trainer}) => (
+    trainer?
+    <div>{trainer.firstName}</div>
+    : <div></div>
 );
 
-export const WorkoutScheduleView = ({workouts}) => (
-    <ol id="shedule_view">
-     {workouts.map(workout => (
-        <li key={workout.startsAt}>{workout.startsAt}</li>))
-     }
-    </ol>)
+const workoutTimeFormat = (time) => {
+    const [h, m] = new Date(time).toTimeString().split(':');
+    return `${h}:${m}`;
+};
+
+export const WorkoutScheduleView = ({workouts}) =>{
+  return  (<div id="container">        
+             <ol id="schedule_view">
+             {workouts.map(workout => 
+                             <li key={workout.startsAt}>{workoutTimeFormat(workout.startsAt)}</li>
+             )}
+            </ol>
+                {
+                    workouts.length < 1?
+                (<p>Nothing planned for today</p>)
+                :
+                (<Booking {...workouts[0]} />                )
+            }
+        
+    </div>)
+    
+}
--- fitness-app/test/Booking.test.js
+++ fitness-app/test/Booking.test.js
@@ -12,23 +12,24 @@ describe('Booking page', () => {
 
     const render = (component) => ReactDOM.render(component, container);     
 
-    it('Renders user\'s first name', ()=> {
+
+    it('Renders trainers\'s first name', ()=> {
         //Arrange
-        const user = { firstName: 'Artem' };
+        const trainer = { firstName: 'Artem' };
 
         //Act
-        render(<Booking user={user} />);
+        render(<Booking trainer={trainer} />);
 
         //Assert
         expect(container.textContent).toMatch('Artem');
     });
 
-    it('Renders first name of another user', ()=> {
+    it('Renders first name of another trainer', ()=> {
         //Arrange
-        const user = { firstName: 'Pavel' };
+        const trainer = { firstName: 'Pavel' };
 
         //Act
-        render(<Booking user={user} />);
+        render(<Booking trainer={trainer} />);
 
         //Assert
         expect(container.textContent).toMatch('Pavel');
@@ -42,17 +43,39 @@ describe('Workouts schedule view', () => {
     })
 
     const today = new Date();        
-    const workoutTimeFormat = (time) => {
-        const [h, m] = new Date(time).toTimeString().split(':');
-        return `${h}:${m}`;
-    };
+    
     const workouts = [
-        {startsAt: workoutTimeFormat(today.setHours(12, 0))},
-        {startsAt: workoutTimeFormat(today.setHours(13, 0))}
+        {startsAt: today.setHours(12, 0)},
+        {startsAt: today.setHours(13, 0)}
     ];
 
     const render = component =>
         ReactDOM.render(component, container);
+
+        it('Initially shows message that there are no workouts today', () => {
+            //Act
+            render(<WorkoutScheduleView workouts={[]} />);
+    
+            //Assert
+            expect(container.textContent).toMatch('Nothing planned for today');
+        });
+    
+    
+        it('Selects first workout by default', ()=> {
+            //Arrange
+            const expectedTrainerName = 'biceps and shoulders' 
+            const workouts = [
+                {
+                    startsAt: today.setHours(12, 0),
+                    trainer: {firstName: expectedTrainerName}
+                }];
+    
+            //Act
+            render(<WorkoutScheduleView workouts={workouts} />)
+    
+            //Assert
+            expect(container.textContent).toMatch(expectedTrainerName)
+        })
 
     it('renders an ol with a right id', () => {
         //Act
@@ -60,7 +83,7 @@ describe('Workouts schedule view', () => {
             workouts={ workouts } />);
 
         //Assert
-        expect(container.querySelector('ol#shedule_view')).not.toBeNull();
+        expect(container.querySelector('ol#schedule_view')).not.toBeNull();
     })
 
     it('renders multiple bookings in ol list', ()=> {
@@ -72,8 +95,7 @@ describe('Workouts schedule view', () => {
             .toHaveLength(2);
     })    
 
-    it('renders each workout in a li', () => {
-        
+    it('renders each workout in a li', () => {        
         // Act
         render(<WorkoutScheduleView workouts={workouts} />);
 
