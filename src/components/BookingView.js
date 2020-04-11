29cc3fc90300f116a36c1a26a7b41be14a855bd8
Artem Tenkov <codefiredeveloper@gmail.com>
2020-04-11 15:02:41 +0200

    UI CSS improvements

--- src/components/BookingView.js
+++ src/components/BookingView.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 
-export const Booking = ({trainer}) => {
+export const Booking = ({trainer, workoutType, notes}) => {
     
     return (
         trainer?
-        <div>
-            <div className="danger">{trainer.firstName}</div>
-            <div>{trainer.lastName}</div>
-            <div>{trainer.phoneNumber}</div>
-            <div>{trainer.lastName}</div>
-        </div>
-        
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Trainer name:</th>
+                    <th scope="col">Trainer last name:</th>
+                    <th scope="col">Trainer phone number:</th>
+                    <th scope="col">Workout type:</th>
+                    <th scope="col">Notes:</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{trainer.firstName}</td>
+                    <td>{trainer.lastName}</td>
+                    <td>{trainer.phoneNumber}</td>
+                    <td>{workoutType}</td>
+                    <td>{notes}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>        
         : <div></div>
     );
 } 
@@ -23,23 +38,30 @@ const workoutTimeFormat = (time) => {
 export const WorkoutScheduleView = ({workouts}) =>{
   const [selectedWorkout, setSelectedWorkout] = useState(0);
 
-  return  (<div id="container">        
-             <ol id="schedule_view">
-             {workouts.map((workout, i) => 
-                             <li key={workout.startsAt}>
-                                <button type="button" onClick={() => setSelectedWorkout(i)}>
+  return  (<div id="container" className="container p-5">     
+                <br />
+                <div className="row">
+                    <div className="col-md-3">
+                    <ul id="schedule_view">
+                        {workouts.map((workout, i) => 
+                             <li class="list-group-item" key={workout.startsAt}>
+                                <button type="button" className="btn btn-secondary" 
+                                 onClick={() => setSelectedWorkout(i)}>
                                     {workoutTimeFormat(workout.startsAt)}
                                 </button>
                              </li>
-             )}
-            </ol>
-                {
+                         )}
+                    </ul>
+                    </div>
+                    <div className="col-md-9">
+                    {
                     workouts.length < 1?
-                (<p>Nothing planned for today</p>)
-                :
-                (<Booking {...workouts[selectedWorkout]} />                )
-            }
-        
+                        (<p>Nothing planned for today</p>)
+                        :
+                        (<Booking {...workouts[selectedWorkout]} />                )
+                    }
+                    </div>
+                </div>   
     </div>)
     
 }
